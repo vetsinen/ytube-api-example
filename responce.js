9ea25bc3ff6b1e7667d89e821b12aa29f2e9ea19
vetsinen <vetsinen@gmail.com>
2021-10-22 02:11:08 +0300

    initial commit

---
+++ responce.js
@@ -0,0 +1,97 @@
+let r = {
+    "kind": "youtube#playlistListResponse",
+    "etag": "RPUZ7VXZkha-f7tBE9j3bbEA4TQ",
+    "pageInfo": {
+        "totalResults": 2,
+        "resultsPerPage": 5
+    },
+    "items": [
+        {
+            "kind": "youtube#playlist",
+            "etag": "LldFM8DQ_WxYJ3oj-fOGqcDqZ78",
+            "id": "PLQGSgPqYbgfUmf30utsEnEN07WPm9Sb69",
+            "snippet": {
+                "publishedAt": "2021-10-21T21:57:04Z",
+                "channelId": "UCLdr_tZ0LERHM9l1B62V3uA",
+                "title": "node.js",
+                "description": "",
+                "thumbnails": {
+                    "default": {
+                        "url": "https://i.ytimg.com/vi/b8k0FLrW4xw/default.jpg",
+                        "width": 120,
+                        "height": 90
+                    },
+                    "medium": {
+                        "url": "https://i.ytimg.com/vi/b8k0FLrW4xw/mqdefault.jpg",
+                        "width": 320,
+                        "height": 180
+                    },
+                    "high": {
+                        "url": "https://i.ytimg.com/vi/b8k0FLrW4xw/hqdefault.jpg",
+                        "width": 480,
+                        "height": 360
+                    },
+                    "standard": {
+                        "url": "https://i.ytimg.com/vi/b8k0FLrW4xw/sddefault.jpg",
+                        "width": 640,
+                        "height": 480
+                    },
+                    "maxres": {
+                        "url": "https://i.ytimg.com/vi/b8k0FLrW4xw/maxresdefault.jpg",
+                        "width": 1280,
+                        "height": 720
+                    }
+                },
+                "channelTitle": "vetsinen openair",
+                "localized": {
+                    "title": "node.js",
+                    "description": ""
+                }
+            },
+            "contentDetails": {
+                "itemCount": 5
+            }
+        },
+        {
+            "kind": "youtube#playlist",
+            "etag": "3farr8pN51SIlEtezalrzOC_QHM",
+            "id": "PLQGSgPqYbgfUR6ofg-dSC_b13kxC2YE0C",
+            "snippet": {
+                "publishedAt": "2021-10-21T21:37:11Z",
+                "channelId": "UCLdr_tZ0LERHM9l1B62V3uA",
+                "title": "main",
+                "description": "",
+                "thumbnails": {
+                    "default": {
+                        "url": "https://i.ytimg.com/vi/sI9p9b5zs44/default.jpg",
+                        "width": 120,
+                        "height": 90
+                    },
+                    "medium": {
+                        "url": "https://i.ytimg.com/vi/sI9p9b5zs44/mqdefault.jpg",
+                        "width": 320,
+                        "height": 180
+                    },
+                    "high": {
+                        "url": "https://i.ytimg.com/vi/sI9p9b5zs44/hqdefault.jpg",
+                        "width": 480,
+                        "height": 360
+                    },
+                    "standard": {
+                        "url": "https://i.ytimg.com/vi/sI9p9b5zs44/sddefault.jpg",
+                        "width": 640,
+                        "height": 480
+                    }
+                },
+                "channelTitle": "vetsinen openair",
+                "localized": {
+                    "title": "main",
+                    "description": ""
+                }
+            },
+            "contentDetails": {
+                "itemCount": 3
+            }
+        }
+    ]
+}
